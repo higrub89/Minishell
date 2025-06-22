a30bf1e74d288372f0577d661efe406901b8b8b8
higrub89 <higuitaruben@hotmail.com>
2025-06-23 01:22:16 +0200

    make re fclean clean all

--- srcs/pruebas.c
+++ srcs/pruebas.c
@@ -2,8 +2,7 @@
 
 int main()
 {
-    char *s = "Hola";
-    char *c = ft_strdup(s);
-    ft_printf("Word = %s\nEsto es una copia = %s\n", s,c);
-    return (0);
+   char *s = "Hola que tal";
+   ft_printf("%s\n", s);
+   return (0);
 }
