32c3941ab07ede8afa7b43909497bf0e9b9b2c87
higrub89 <higuitaruben@hotmail.com>
2025-08-02 19:54:51 +0200

    correccion funcion find_command_path del executor.h

--- inc/executor.h
+++ inc/executor.h
@@ -11,6 +11,6 @@
 //int execute_commands(t_command *head_cmd);
 int handle_redirecctions(t_redirection *redirs, char **envp);
 int execute_external_command(t_command *cmd, char **envp, int *last_exit_status);
-char *find_command_path(char *cmd_name);
+char *find_command_path(char *cmd_name, char **envp);
 
 #endif
